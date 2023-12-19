cf0208266ef5619ab13658f4287aa21e35270491
Tyler Cav <tyler.cavanaugh24@gmail.com>
2023-12-18 21:06:50 -0500

    built logo functions, svg tag for square working.

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
 const inquirer = require('inquirer');
+//How to remove color package?
 const Color = require('color');
-//const generateSVG = require('./');
+
+//TODO: ASK ABOUT xmlns, I still don't fully understand the purpose. 
+
+function createCircle (response) {}
+
+function createTriangle (response) {}
+
+function createSquare (response) {
+    const Square = `<svg width="300px" height="200px" xmlns="http://www.w3.org/2000/svg">
+    <rect width="300px" height="200px" fill="${response.shapeColor}" stroke="black" stroke-width="6"/>
+    <text x="50%" y="50%">${response.textCharacters}</text>
+    </svg>`;
+    return Square
+}
+
+function svgTemplate (response) {
+    if (response.shape === "Circle") {
+        return createCircle(response)
+    } else if (response.shape === "Triangle") {
+        return createTriangle(response) 
+    } else {
+        return createSquare(response)
+    }
+}
+
 
 //prompts build to be used as values for the readme file
 inquirer
@@ -40,6 +65,6 @@ inquirer
     //the generateMarkdown keyword is calling to the generateMarkdown.js function which provides details of the prompt responses.
         .then(response => {
             console.log(response)
-        //fs.writeFile('Dist/logo.svg', generateMarkdown(response), (err) => 
-        //err ? console.error(err) : console.log('Success!'))
+        fs.writeFile('SVG-Dist/logo.svg', svgTemplate(response), (err) => 
+        err ? console.error(err) : console.log('Success!'))
     })
